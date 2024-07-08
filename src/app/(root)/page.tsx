1effc9232646e1038526b7f1d3f53bbed4527052
Adam Slíva <adamsliva1@seznam.cz>
2024-07-08 11:44:52 +0200

    init/Project initialization

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="bg-base-100 h-screen p-16 font-bold">Data tady</div>
+    </>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import AddIcon from "@material-symbols/svg-400/outlined/add.svg";
+import HistoryIcon from "@material-symbols/svg-400/outlined/history.svg";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-200 h-16 shadow-md z-10 sticky top-0">
+      <div className="flex gap-4">
+        <Link href="/" className="text-xl font-bold flex mx-4">
+          Uzávěrky<div className="text-emerald-700">Online</div>
+        </Link>
+        <div className="flex gap-4 py-2 [&>a]:btn [&>a]:btn-outline [&_svg]:h-8 [&_svg]:w-8">
+          <Link href="/" className="btn-success">
+            <AddIcon />
+            Nová denní uzávěrka
+          </Link>
+          <Link href="/">
+            <HistoryIcon />
+            Historie uzávěrek
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
